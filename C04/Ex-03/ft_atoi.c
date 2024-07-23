c283366aa13a26c65270baa00893484d66990c2e
Gael Pilet <gpilet@c2r5p4.42perpignan.fr>
2024-07-23 18:57:54 +0200

    C-04 Ex00-03

--- C04/Ex-03/ft_atoi.c
+++ C04/Ex-03/ft_atoi.c
@@ -5,9 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gpilet <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/23 14:48:45 by gpilet            #+#    #+#             */
-/*   Updated: 2024/07/23 14:48:50 by gpilet           ###   ########.fr       */
+/*   Created: 2024/07/23 18:06:02 by gpilet            #+#    #+#             */
+/*   Updated: 2024/07/23 18:53:34 by gpilet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_atoi(char *str)
+{
+	int	sum;
+	int	sign;
 
+	sum = 0;
+	sign = 0;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		sum = sum * 10 + (*str - '0');
+		str++;
+	}
+	return (sign * sum);
+}
+
+/*#include <stdio.h>
+
+int	main(void)
+{
+	char s[]= "   ---+--+1234ab567";
+
+	printf("%d", ft_atoi(s));
+	return (0);
+}*/
