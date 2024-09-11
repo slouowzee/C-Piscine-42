c8ccd081778fab15d34e808a57fe223c8f50fbd9
SlouSlou <gael.pilet1@gmail.com>
2024-09-11 23:28:26 +0200

    Update ft_fibonacci.c

--- C05/Ex-04/ft_fibonacci.c
+++ C05/Ex-04/ft_fibonacci.c
@@ -12,21 +12,12 @@
 
 int	ft_fibonacci(int index)
 {
-	int	sum1;
-	int	sum2;
-	int	temp;
-
-	sum1 = 0;
-	sum2 = 1;
 	if (index < 0)
 		return (-1);
 	if (index == 0)
 		return (0);
 	if (index == 1)
 		return (1);
-	temp = sum2;
-	sum2 += sum1;
-	sum1 = temp;
 	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
 
